5a2f2e4c631bd01c75472429331798d955287dce
Punya Taluka <70637133+PunyaTaluka123@users.noreply.github.com>
2024-06-07 14:09:11 +0530

    Calculator works only in console

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
-function add(num1, num2){
-    return num1+num2
-}
-
 function subtract(num1, num2){
     return num1-num2
 }
-
 function divide(num1, num2){
     if (num2===0){
         return 'No'
     }
     return num1/num2
 }
-
 function multiply(num1, num2){
     return num1*num2
 }
 
+function add(num1, num2) {
+    return num1 + num2;
+}
+function calculate(num1, num2, operator){
+    switch(operator){
+        case '+':
+            console.log(add(num1, num2))
+            return
+        case '-':
+            console.log(subtract(num1, num2))
+            return
+        case '*':
+            console.log(multiply(num1, num2))
+        case "/":
+            console.log(divide(num1, num2))
+            return
+    }
+}
+function getNumber(num){
+    return Number(num)
+}
+function getOperator(op){
+    return op
+}
+
+calculate(getNumber, getNumber,getOperator, )
