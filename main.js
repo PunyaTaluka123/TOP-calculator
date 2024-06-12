a824cc3a08c18c2ee5a2a575786057e617bbfba6
Punya Taluka <70637133+PunyaTaluka123@users.noreply.github.com>
2024-06-12 12:06:32 +0530

    made the skeleton of caluclator

--- main.js
+++ main.js
@@ -1,39 +1,37 @@
+let num1
+let operator
+let num2
+
+function operate(num1, operator, num2){
+    switch (operator){
+        case "+":
+            return add(num1, num2)
+        case "-":
+            return subtract(num1, num2)
+        case "*":
+            return multiply(num1, num2)
+        case "/":
+            return divide(num1, num2) 
+    }
+}
+
+
+
+
+
+
+function add(num1, num2){
+    return num1 + num2
+}
+
 function subtract(num1, num2){
     return num1-num2
 }
-function divide(num1, num2){
-    if (num2===0){
-        return 'No'
-    }
-    return num1/num2
-}
+
 function multiply(num1, num2){
     return num1*num2
 }
 
-function add(num1, num2) {
-    return num1 + num2;
+function divide(num1, num2){
+    return num1/num2
 }
-function calculate(num1, num2, operator){
-    switch(operator){
-        case '+':
-            console.log(add(num1, num2))
-            return
-        case '-':
-            console.log(subtract(num1, num2))
-            return
-        case '*':
-            console.log(multiply(num1, num2))
-        case "/":
-            console.log(divide(num1, num2))
-            return
-    }
-}
-function getNumber(num){
-    return Number(num)
-}
-function getOperator(op){
-    return op
-}
-
-calculate(getNumber, getNumber,getOperator, )
